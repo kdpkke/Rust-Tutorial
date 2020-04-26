069d2db91ed846876bacee050eca17890cdf9203
Matteo <matteo.muratore-ext@alstomgroup.com>
2020-04-26 18:19:21 +0200

    Moduli 1 - 5 (compreso).
    Co-authored-by: Michele Paci <miche.paci@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,31 @@ const CIAO:u8 = 42; //"let" non funziona per le variabili globali. La var global
 
 static mut NOT_SAFE:u16 = 123; //con static è possibile usare "mut" per poter cambiare il valore della variabile, ma per modificare il valore bisogna scriverte codice in un blocco chiamato "unsafe"
 
+enum Color
+{
+    Red,
+    Green,
+    Blue,
+    RgbColor(u8,u8,u8),
+    Cmyk{cyan:u8, magenta:u8, yellow:u8, black:u8}
+}
+
+fn enums()
+{
+    let c:Color = Color::Cmyk{cyan: 0, magenta: 128, yellow: 0, black: 0};
+    
+    match c
+    {
+        Color::Red => println!("R"),
+        Color::Green => println!("G"),
+        Color::Blue => println!("B"),
+        Color::RgbColor(0,0,0) | Color::Cmyk{cyan:_, magenta:_, yellow:_, black:255} => println!("black"), //si possono mettere in OR le due condizioni e l'underscore indica che ci può essere qualsiasi tipo di valore in quell'oggetto della struct. 
+        Color::RgbColor(r,g,b) => println!("rgb({},{},{})", r, g, b),
+                                                                                                         
+        _ => println!("invalid")
+    };
+}
+
 fn prova_var_globale()
 {
     unsafe
@@ -13,6 +38,141 @@ fn prova_var_globale()
         NOT_SAFE = 456;
         println!("global NOT_SAFE: {}", NOT_SAFE);
     }
+}
+
+fn option()
+{
+    let x = 3.0;
+    let y = 2.0;
+
+    let result:Option<f64> = if y != 0.0 {Some(x/y)} else { None };
+
+    match result
+    {
+        Some(z) => println!("{}/{} = {}", x,y,z),
+        None => println!("cannot divide {} by {}", x,y)
+    };
+
+    //fare match result come fatto sopra e fare if let...else come fatto sotto è la stessa cosa. La differenza è che nel match devi obbligatoriamente
+    //includere tutti i possibili risultati, mentre con if let NO.
+    if let Some(z) = result { println!("{}/{} = {}", x,y,z) } //questo println viene eseguito solo se result ha "valore" Some. Se result ha valore "None", non viene eseguita la println.
+    else { println!("cannot divide {} by {}", x,y) }; 
+}
+
+fn arrays()
+{
+    let mut arr:[i32;5] = [1,2,3,4,5];
+    let mut arr2 = [2,3,4,5,6];
+
+    println!("arr has {} elements, first is {}", arr.len(), arr[0]);
+    println!("{:?}", arr); //stampa tutto l'array
+
+    
+    if arr2 == [1,2,3,4,5]
+    {
+        println!("array is equal to the other one.");
+    }
+
+    let arr3 = [1u16; 10]; //riempie un array grande 10 con dieci "1" u16.
+
+    println!("arr3 takes up {} bytes", mem::size_of_val(&arr3));
+
+    let mtx:[[f32;3]; 2] = //3 sono le colonne, 2 le righe.
+    [
+        [1.0, 0.0, 0.0],
+        [0.0, 2.0, 0.0]
+    ];
+    println!("{:?}", mtx);
+}
+
+fn vectors()
+{
+    let mut a = Vec::new();
+    a.push(1);
+    a.push(2);
+    a.push(3);
+    a.push(4);
+
+    match a.get(6) //get ritorna un Option così da essere memory safe nel caso in cui l'index sia out of range.
+    {
+        Some(x) => println!("a[3] = {}", x),
+        None => println!("error, no such element")
+    };
+
+    for x in &a { println!("{}", x);} //serve la & per accedere a tutti gli element del vector.
+
+    while let Some(x) = a.pop() //stampa tutto il vettore al contrario togliendo nel frattempo tutti i valori al suo interno.
+    {
+        println!("{}",x); 
+    }
+}
+
+fn use_slice(slice: &mut[i32])
+{
+    println!("first elem = {}, len = {}", slice[0], slice.len());
+    slice[0] = 4321
+}
+fn slices()
+{
+    let mut data = [1,2,3,4,5];
+
+    use_slice(&mut data[1..4]); //gli passo solo gli elementi in mezzo dell'array
+    use_slice(&mut data); //gli passo tutto l'array
+
+    println!("{:?}", data);
+}
+
+fn strings()
+{
+    let s:&'static str = "hello there!"; // &str = string slice. Si alloca nello Heap.
+    //s = "abc"; ERRORE
+    //let h = s[0]; ERRORE
+
+    for c in s.chars()
+    {
+        println!("{}", c); // stampa tutti i caratteri 
+    }
+
+    for c in s.chars().rev()
+    {
+        println!("{}", c); // stampa tutti i caratteri in reverse
+    }
+
+    if let Some(first_char) = s.chars().nth(0)
+    {
+        println!("first letter is: {}", first_char)
+    }
+
+    let mut letters = String::new();
+    let mut a = 'a' as u8;
+    while a <= ('z' as u8)
+    {
+        letters.push(a as char);
+        letters.push_str(",");
+        a += 1;
+    }
+
+    println!("{}", letters);
+
+    //concatenation String + str è possibile. non è possibile concatenare String + String.
+    //let z = letters + &letters --> non funziona
+    //&str <> string clone, se prendo &letters direttamente mi da errore alla concatenazione (come se facendo &letters venisse modificata la variabile letters in tutte le sue chiamate)
+    let u:&str = &letters.clone();
+    let z = letters + u; //funziona
+    println!("concatenation of the same string {}", z);
+}
+
+fn tuples()
+{
+    let sp = (4, true); //i tipi all'interno dei tuples può essere diverso.
+    println!("First elem: {}, Scond elem: {}", sp.0, sp.1);
+    //salvo in a e b rispettivamente gli elementi 0, 1
+    let (a, b) = sp;
+    println!("First elem: {}, Scond elem: {}", a, b);
+
+    //tuple con un solo elemento
+    let meaning = (42,);
+    println!("tuple single element: {:?}", meaning);
 }
 
 fn main() {
@@ -57,4 +217,34 @@ fn main() {
     prova_var_globale();
 
     stack_heap::stack_and_heap();
+    
+    for x in 1..11 //da 1 a 10. 11 NON è incluso 
+    {
+        println!("x = {}", x);
+    }    
+
+    for (pos,val) in (30..41).enumerate() //questo for va da 0 a 10 per "pos" e va da 30 a 40 per "val"
+    {
+        println!("pos: {}, y: {}", pos, val);
+    }
+
+    let country_code = 77;
+
+    let country = match country_code //è come fare switch..case inline.
+    {
+        44 => "UK",
+        77 => "russia",
+        1..=999 => "unknown", //in questo caso, a differenza del range del for, ..= tra i due numeri indica un range inclusivo da 1 a 999 (compreso)  
+        _ => "invalid" 
+    };
+
+    println!("the country with code {} is {}", country_code, country);
+
+    enums();
+    option();
+    arrays();
+    vectors();
+    slices();
+    strings();
+    tuples();
 }
